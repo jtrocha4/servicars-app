0bec1129c829f363bbc1b415ec47e9effe6294c9
jtrocha4 <jtrocha04@gmail.com>
2024-03-22 17:47:52 -0500

    update(main)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { WorkOrderProvider } from './contexts/workOrder.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WorkOrderProvider>
+      <App />
+    </WorkOrderProvider>
   </React.StrictMode>
 )
